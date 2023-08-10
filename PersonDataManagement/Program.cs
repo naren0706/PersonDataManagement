f4b8862ceb85fa880789e49e1267fb8638c22e06
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-08-10 10:17:27 +0530

    [Narenthra Kishore] Add : new values for list

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace PersonDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>();
+            list.Add(new Person()
+            {
+                SNN = 1,
+                Name = "a",
+                Address = "a",
+                Age = 18
+            });
+            list.Add(new Person()
+            {
+                SNN = 2,
+                Name = "b",
+                Address = "b",
+                Age = 18
+            });
+            list.Add(new Person()
+            {
+                SNN = 3,
+                Name = "c",
+                Address = "c",
+                Age = 18
+            });
+        }
+    }
+}
