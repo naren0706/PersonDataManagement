d0bf7d67ed23539663bd2275f1bd3d43ac664cbc
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-08-10 10:28:56 +0530

    [Narenthra Kishore] Add : new feature to get top 2 values and age less than 60

--- PersonDataManagement/Operation.cs
+++ PersonDataManagement/Operation.cs
@@ -1,9 +1,22 @@
-﻿namespace PersonDataManagement
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PersonDataManagement
 {
     internal class Operation
     {
-        public Operation()
+        public void RetreiveTopTwoRecords(List<Person> list)
         {
+            var result = list.Where(x => x.Age < 60).Take(2).ToList();
+            Display(result);
+        }
+
+        private void Display(List<Person> result)
+        {
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Name+" | " +item.Address+ " | "+item.Age );
+            }
         }
     }
 }
--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -5,28 +5,30 @@ namespace PersonDataManagement
     {
         static void Main(string[] args)
         {
-            List<Person> list = new List<Person>();
-            list.Add(new Person()
+            List<Person> li = new List<Person>();
+            li.Add(new Person()
             {
                 SNN = 1,
                 Name = "a",
                 Address = "a",
                 Age = 18
             });
-            list.Add(new Person()
+            li.Add(new Person()
             {
                 SNN = 2,
                 Name = "b",
                 Address = "b",
                 Age = 18
             });
-            list.Add(new Person()
+            li.Add(new Person()
             {
                 SNN = 3,
                 Name = "c",
                 Address = "c",
                 Age = 18
             });
+            Operation operation = new Operation();
+            operation.RetreiveTopTwoRecords(li);
         }
     }
 }
